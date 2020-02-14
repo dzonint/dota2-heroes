86cc6789fdb0db943280ee2d6351b75cd23f5eb0
dzonint <dzonint@yahoo.com>
2020-02-14 03:57:29 +0100

    Updates

---
+++ precache-manifest.9e4bcc0473521ceae6fc176d2f4e9f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6159eec5e332eeebe5315086d1bddc78",
+    "url": "/dzonint/dota2-heroes/index.html"
+  },
+  {
+    "revision": "ef77229ab66361573b49",
+    "url": "/dzonint/dota2-heroes/static/css/main.e4a72cd8.chunk.css"
+  },
+  {
+    "revision": "d68e3e0c1b112c441bc2",
+    "url": "/dzonint/dota2-heroes/static/js/2.105d1c69.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dzonint/dota2-heroes/static/js/2.105d1c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef77229ab66361573b49",
+    "url": "/dzonint/dota2-heroes/static/js/main.f23e3696.chunk.js"
+  },
+  {
+    "revision": "dfd116ba7a46e23ba2fb",
+    "url": "/dzonint/dota2-heroes/static/js/runtime-main.e7c79596.js"
+  }
+]);
